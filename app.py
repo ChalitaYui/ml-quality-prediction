eafefdfe42b1f2aa16b4cbaee5ec0ad6e7757d6c
ChalitaYui <lekchaumchalita@gmail.com>
2026-02-07 15:53:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
 
+app = FastAPI()
+
+model = joblib.load("model.joblib")
+
+@app.get("/predict")
+def predict(x: float):
+    prediction = model.predict(np.array([[x]]))
+    return {
+        "input": x,
+        "prediction": prediction[0]
+    }
