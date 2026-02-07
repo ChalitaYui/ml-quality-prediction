d066cfd10c39bf91df063140b3bbfb60e9c46511
ChalitaYui <lekchaumchalita@gmail.com>
2026-02-07 14:56:38 +0700

    Create train.py

---
+++ train.py
@@ -0,0 +1,12 @@
+import joblib
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+X = np.array([[1],[2],[3],[4],[5]])
+y = np.array([2,4,6,8,10])
+
+model = LinearRegression()
+model.fit(X, y)
+
+joblib.dump(model, "model.joblib")
+print("Model trained and saved")
